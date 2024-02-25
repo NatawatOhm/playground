1f0bda5fa8708e200404798acbd185865bb3ec1d
NatawatOhm <natawat.cho@gmail.com>
2024-02-25 14:04:59 +0700

    [add] pinia store

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,16 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+//pinia
+import { createPinia } from "pinia";
 
+const app = createApp(App);
 const vuetify = createVuetify({
   components,
   directives,
 });
-const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
 app.use(vuetify);
 app.mount("#app");
