d7c29d156ec284d3e52f5b703af2bcb848d60bcd
NatawatOhm <natawat.cho@gmail.com>
2024-02-25 14:24:08 +0700

    [add] router from vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
+import router from "./router";
 import App from "./App.vue";
 // Vuetify
 import "vuetify/styles";
@@ -17,5 +18,6 @@ const vuetify = createVuetify({
 const pinia = createPinia();
 
 app.use(pinia);
+app.use(router);
 app.use(vuetify);
 app.mount("#app");
